ada9af21cae1536b06df871da19476fd4acd60d3
vnovopashin <novopashinva@gmail.com>
2023-03-07 21:26:58 +0700

    6. Создание, просмотр, редактирование и удаление кандидатов [#504858 #325774]

--- src/main/java/ru/job4j/dreamjob/Main.java
+++ src/main/java/ru/job4j/dreamjob/Main.java
@@ -13,5 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        System.out.println("Go to http://localhost:8081/index");
     }
 }
--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -14,7 +14,10 @@ public class Candidate {
     private int id;
     private String name;
     private String description;
-    private LocalDateTime creationDate;
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    public Candidate() {
+    }
 
     public Candidate(int id, String name, String description, LocalDateTime creationDate) {
         this.id = id;
